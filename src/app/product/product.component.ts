8833973de1a555f1846a18fef6cc0bacf148e3de
younes amara <younes.amara@etu.umontpellier.fr>
2024-05-30 23:30:31 +0200

    fixed errors

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {FooterComponent} from "../footer/footer.component";
+import {HeaderComponent} from "../header/header.component";
+import {ProductService} from "../services/product.service";
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  standalone: true,
+  imports: [
+    FooterComponent,
+    HeaderComponent
+  ],
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+  product: any;
+
+  constructor(private route: ActivatedRoute, private productService: ProductService) {
+  }
+
+  ngOnInit(): void {
+    const productId = this.route.snapshot.paramMap.get('id');
+    this.product = this.productService.getProductById(Number(productId));
+  }
+}
